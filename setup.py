8d86ee89cde55d492929decced46671a7965f706
EpicShardGaming YT <35035079+EpicShardGamingYT@users.noreply.github.com>
2019-03-27 19:23:06 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pyntube",
+    version="0.0.1",
+    author="Epic",
+    author_email="surtla100@gmail.com",
+    description="Python streaming service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tag-epic/pyntube",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
